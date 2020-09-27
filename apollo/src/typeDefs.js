48451f1840622bc8454b5047288a0f229c6d6109
paweemew <pawee.tanti@gmail.com>
2020-09-27 13:43:24 +0700

    add some data

--- apollo/src/typeDefs.js
+++ apollo/src/typeDefs.js
@@ -3,50 +3,47 @@ import { gql } from "apollo-server";
 export default gql`
   scalar Date
 
-  type User{
+  type User {
     _id: ID!
     username: String!
+    trucks: [UsersTruck]
   }
 
   type Token {
     token: String!
   }
 
-  type Truck {
-    _id: ID,
-    owner: String!,
-    truck_id: String!,
-    lat: Float!,
-    lng: Float!,
-    temp: Float!,
-    date: Date
+  type UsersTruck {
+    _id: ID!
+    user: User!
+    truckId: String!
+    trucksData: [TrucksData]
+  }
+
+  type TrucksData {
+    _id: ID!
+    truck: UsersTruck!
+    date: Date!
+    temp: Float!
+    lat: Float!
+    lng: Float!
   }
 
   type Query {
     user: User!
     login(username: String!, password: String): Token!
-    trucks: [Truck]
-  }
-
-  input DateInput{
-    date: Date
-  }
-
-  input TruckInput {
-    owner: String,
-    truck_id: String!,
-    lat: Float!,
-    lng: Float!,
-    temp: Float!,
-    date: Date
+    trucksData(truckId: String!): [TrucksData]
   }
 
   type Mutation {
-    createUser(
-      username: String!,
-      password: String!
-    ): User!
-
-    createTruck(input: TruckInput): Truck
+    createUser(username: String!, password: String!): User!
+    addUsersTruck(truckId: String!): UsersTruck!
+    addTrucksData(
+      truckId: String!
+      date: Date
+      temp: Float!
+      lat: Float!
+      lng: Float!
+    ): TrucksData!
   }
 `;
